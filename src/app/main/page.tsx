b8bfc03d227aa2e8ab3a4606df670dc4e8842a1a
j4zuu <jasu.honkanen2@gmail.com>
2023-10-24 23:55:48 +0300

    Rakenneetta jälleen vähän muutettu. Pääsivu työn alla

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,12 +1,28 @@
 "use client"
 
 import React from "react";
+import TopBar from "@/app/components/TopBar";
+import Feed from "@/app/components/Feed";
+import TempCard from "@/app/components/TempCard";
+
+const temp1: number = 1
+const temp2: number = 2
+const temp3: number = 3
+const temp4: number = 4
 
 const Page = () => {
-  return (
-      <div className="login-container">
-          This is the main page after logging in
-      </div>
-  )
+    return (
+        <div className="mainArea">
+            <TopBar/>
+            <Feed/>
+            <div className="base">
+                <TempCard temp={temp1}/>
+                <TempCard temp={temp2}/>
+                <TempCard temp={temp3}/>
+                <TempCard temp={temp4}/>
+            </div>
+
+        </div>
+    )
 }
 export default Page
