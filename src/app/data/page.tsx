d56a92253340860849f6cd58d319e435f3a7c345
mmmerili <75306943+mmmerili@users.noreply.github.com>
2023-11-28 23:59:01 +0200

    Update page.tsx

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import TopBar from "@/app/components/TopBar";
 import ChartComponent from "../components/ChartComponent";
 import Feed from "../components/Feed";
-import "../styles.css";
 
 
 const Page = () => {
